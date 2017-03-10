3c54b85cf6ba5da0a54c447d79801f1d859412ff
wengwengweng <593110501@qq.com>
2017-03-11 01:06:17 +0800

    查询单词api实现调用

--- myscript.js
+++ myscript.js
@@ -1,25 +1,30 @@
-// $('#bannerandheader').hide();
-// $('#google_image_div').hide();
-// $('.js-components-container').hide();
-// $('.content__meta-container.js-content-meta.js-football-meta.u-cf').hide();
-// $('.element.element-rich-link.element--thumbnail.element-rich-link--upgraded').hide();
-// $('.content__labels.content__labels--not-immersive').hide();
-// $('.content__secondary-column.js-secondary-column').hide();
-// $('.rich-link').hide();
-// $('.content-footer').hide();
-// $('.submeta').hide();
-// $('footer').hide();
-// $('body').click(function(){
-//   chrome.extension.sendRequest({greeting: "hello"}, function(response) {
-//     console.log(response);
-//   });
-// })
-// chrome.extension.onRequest.addListener(
-//   function(request, sender, sendResponse) {
-//     console.log('this is myscript,get the message');
-//   }
-// );
+// 选中翻译
+$('body').mouseup(function(){
+  var selObj = window.getSelection();
+  console.log(selObj.toString());
+  console.log(selObj);
+  if(selObj.toString()){
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "text/plain");
+    myHeaders.append("Authorization", "Bearer TOKEN");
+    fetch("https://api.shanbay.com/bdc/search/?word="+selObj.toString(),
+      {
+        method: "GET"
+      },
+      myHeaders
+    ).then(
+      function(res){
+        return res.json();
+      }
+    ).then(
+      function(data){
+        console.log(data);
+      }
+    )
+  }
+})
 
+  // 监听popup点击事件
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(request);
@@ -52,11 +57,19 @@ chrome.runtime.onMessage.addListener(
       console.log(request.text);
       sendResponse({message: "filter success"});
     }else if (request.type === 'paging' && request.text === true){
-      $('.content__article-body.from-content-api.js-article__body').click({
-        function(){
-          console.log(111);
-        }
-      })
+
+      //添加分页
+      let num = [1,2,3,4],
+          pagingList,
+          pagingUl;
+
+      pagingList = String(num.map(function(value){
+        return '<li key='+value+' style="float:left;text-decoration:none;margin:10px;curosr:pointer;">'+value+'</li>'
+      })).replace(/,/g,'');
+      pagingUl = '<div class="pagingList" style="display:flex;justify-content:center;align-items:center;"><ul>'+pagingList+'</ul></div>';
+
+      console.log(String(pagingList).replace(/,/g,''));
+      $('.content__article-body.from-content-api.js-article__body').append(pagingUl);
 
       sendResponse({message: "paging success"});
     }
